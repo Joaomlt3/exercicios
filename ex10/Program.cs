3d9383664840634e6d0108989e30924ab2d648fc
joaomlt3 <programacaojoao62@gmail.com>
2026-06-01 14:16:07 -0300

    exercicio

---
+++ ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿string nome;
+int chamada;
+
+Console.WriteLine("Digite o seu nome completo :");
+nome = Console.ReadLine();
+Console.WriteLine("Digite o seu número de chamada :");
+chamada = int.Parse(Console.ReadLine());
+if( chamada<= 15){
+    Console.WriteLine($"{nome},seu projeto será focado em Desenvolvimento Web.");
+}
+else
+{ 
+    Console.WriteLine($"{nome},seu projeto será focado em Banco de Dados.");
+}
